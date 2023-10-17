a76495dea87bd1ac295f5bca25d4aa9b6ab04ce1
SKEGDEV <skegdeveloper@gmail.com>
2023-10-16 23:23:54 -0600

    v2.3

--- src/route_obj/o_Rpt.py
+++ src/route_obj/o_Rpt.py
@@ -1,3 +1,4 @@
+from os import error
 from src.util.tools import tools
 from src.util.database import DB
 
@@ -5,7 +6,7 @@ class oRpt:
 
     def __init__(self, params):
         self.params = params
-        self.spList = ['sp_get_qualifications_data', 'sp_get_qualification_header']
+        self.spList = ['sp_get_qualifications_data', 'sp_get_qualification_header', 'sp_get_header_cotejo_rpt', 'sp_get_cotejo_student_list', 'sp_get_cotejo_info', 'sp_get_header_aprettiationRpt', 'sp_get_aprettiation_qualificationList']
         self.getInfoErr = 'Ocurrio un error al tratar de obtener la informacion, por favor contacte a soporte de IT si el error persiste'
         self.defaultErr = 'No se encontro una variante para el reporte solicitado, por favor contacte a soporte de IT si el error persiste'
 
@@ -35,8 +36,64 @@ class oRpt:
                     'q_header':o_header.get('data'),
                     'o_qualification':o_qualification.get('data')
                     }
+        if(self.params["rpt_type"] == 'ct'):
+            o_CTId = []
+            o_information = DB().exec_query(self.spList[4], [self.params['rpt_clist_id']])
+            o_ctHeader = DB().exec_query(self.spList[2], [self.params['rpt_clist_id'], self.params['rpt_unit_number']])
+            if(o_ctHeader.get('err') or o_information.get('err')):
+                return{
+                        'msm':'error',
+                        'err':self.getInfoErr
+                        }
+            o_newHeader = self.MakeNewHeader(o_ctHeader.get('data'))
+            for d in o_newHeader:
+                o_CTId.append(d[0])
+            o_Body = self.MakeoBodyCT(o_CTId) 
+            return{
+                    'msm':'success',
+                    'o_header':o_newHeader,
+                    'o_body':o_Body,
+                    'o_info':o_information.get("data"),
+                    'year':self.params['rpt_year']
+                    }
+        if(self.params["rpt_type"] == 'ap'):
+            o_APinformation = DB().exec_query(self.spList[4], [self.params['rpt_clist_id']])
+            o_APheader = DB().exec_query(self.spList[5], [self.params['rpt_clist_id']])
+            o_APbody = DB().exec_query(self.spList[6], [self.params['rpt_listId'], self.params['rpt_clist_id']])
+            if(o_APinformation.get('err') or o_APheader.get('err') or o_APbody.get('err')):
+                return{
+                        'msm':'error',
+                        'err':self.getInfoErr
+                        }
+            return{
+                    'msm':'success',
+                    'o_header':o_APheader.get('data'),
+                    'o_body':o_APbody.get('data'),
+                    'o_info':o_APinformation.get('data'),
+                    'year':self.params['rpt_year']
+                    }
+
         return{
                 'msm':'error',
                 'err':self.defaultErr
                 }
+
+    def MakeoBodyCT(self, item:list):
+        if(len(item) < 5):
+            i = len(item) - 1
+            while(i < 5):
+                item.append(0)
+                i += 1
+        o_Data = DB().exec_query(self.spList[3], [item[0], item[1], item[2], item[3], item[4], self.params['rpt_clist_id'], self.params['rpt_unit_number']])
+        if(o_Data.get('err')):
+            return []
+        return o_Data.get("data")
+
+    def MakeNewHeader(self, item):
+        if(len(item) < 5):
+            i = len(item) - 1
+            while(i < 5):
+                item.append((0, '', 0, '', 0, '', 0))
+                i += 1
+        return item
 
--- src/route_obj/o_activity.py
+++ src/route_obj/o_activity.py
@@ -3,16 +3,24 @@ from src.util.database import DB
 class o_activity:
 
     def create_activity(self, params): 
-        if(self.isActivityAvailable(int(params["Clist"]), int(params["number"]), float(params["qualification"]))):
+        declarative = params["declarative"]
+        attitudinal = params["attitudinal"]
+        procedural = params["procedural"]
+        activity = params["activity"]
+        qualification = float(declarative["qualification"]) + float(attitudinal["qualification"]) + float(procedural["qualification"])
+        if(self.isActivityAvailable(int(activity["Clist"]), int(activity["number"]), qualification)):
             return{"msm":"La calificacion excede el limite de 100 pts", "isMax":"true"}
         sp = "sp_create_activity"
         o_Result = DB().exec_query(sp, [
-                                        params["name"],
-                                        params["qualification"],
-                                        params["Clist"],
-                                        params["type"],
-                                        params["subtype"],
-                                        params["number"]
+                                        declarative["name"],
+                                        procedural["name"],
+                                        attitudinal["name"],
+                                        declarative["qualification"],
+                                        procedural["qualification"],
+                                        attitudinal["qualification"],
+                                        activity["Clist"],
+                                        activity["subtype"],
+                                        activity["number"]
                                         ])
         if(not o_Result.get("err")):
             return{"msm":"se ha creado con exito la actividad"}
@@ -63,18 +71,51 @@ class o_activity:
         return o_Result
 
     def qualified(self, params):
-        if((float(params["s_points"])+float(params["qualification"]))>float(params["a_points"]) and int(params["type"]) == 1):
-            return{"msm":("El punteo no puede ser mayor a: "+str(float(params["a_points"])-float(params["s_points"]))), "isValid":"false"}
-        if(float(params["s_points"])<float(params["qualification"]) and int(params["type"])==2):
-            return{"msm":("El punteo no puede ser menor que: "+str(params["s_points"])), "isValid":"false"}
-        sp = "sp_update_activity"
+        declarative = params["declarative"]
+        procedural = params["procedural"]
+        attitudinal = params["attitudinal"]
         qualification = params["qualification"]
-        if(int(params["type"]) == 2):
-            qualification = float(params["qualification"]) * (-1)
-        o_Result = DB().exec_query(sp, [params["id"], qualification])
+        isDeclarativeValid = self.isQualificationValid(declarative, int(qualification["type"]))
+        isAttitudinalValid = self.isQualificationValid(attitudinal, int(qualification["type"]))
+        isProceduralValid = self.isQualificationValid(procedural, int(qualification["type"]))
+        if(isDeclarativeValid.get("result") != "success"):
+            return isDeclarativeValid
+        if(isAttitudinalValid.get("result") != "success"):
+            return isAttitudinalValid
+        if(isProceduralValid.get("result") != "success"):
+            return isProceduralValid
+        sp = "sp_update_activity"
+        q_declarative = float(declarative["s_points"])
+        q_attitudinal = float(attitudinal["s_points"])
+        q_procedural = float(procedural["s_points"])
+        if(int(qualification["type"]) == 2):
+            q_declarative *= -1
+            q_attitudinal *= -1
+            q_procedural *= -1
+        o_Result = DB().exec_query(sp, [qualification["id"], q_declarative, q_attitudinal, q_procedural])
         if(not o_Result.get("err")):
             return{
                     "msm":"Se ha calificado correctamente"
                     }
         return o_Result
+
+    def isQualificationValid(self, item, a_type:int):
+        studentPoints = float(item["s_points"])
+        studentCurrentPoints = float(item["s_current_points"])
+        activityQualification = float(item["a_qualification"])
+        if(((studentPoints + studentCurrentPoints) > activityQualification) and a_type == 1):
+            return{
+                    "isValid":"false",
+                    "msm":("La calificacion "+item["q_name"]+" no puede ser mayor a: " + str((activityQualification - studentCurrentPoints))),
+                    "result":"fail"
+                    }
+        if(studentCurrentPoints < studentPoints and a_type == 2):
+            return{
+                    "isValid":"false",
+                    "msm":("La calificacion "+item["q_name"]+" no puede ser menor a: " + str(studentCurrentPoints)),
+                    "result":"fail"
+                    }
+        return{
+                "result":"success"
+                }
 
